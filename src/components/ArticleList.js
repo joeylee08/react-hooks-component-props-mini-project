44e48f871073ea06a2eefae81b23fb81913e5ee7
Joseph <josephylee72@gmail.com>
2023-10-02 18:25:41 -0700

    completed props mini project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Article from './Article.js'
+
+function ArticleList({posts}) {
+  const mapped = posts.map(item => {
+    return <Article title={item.title} date={item.date} preview={item.preview} key={item.id}/>
+  })
+  return (
+    <main>
+      {mapped}
+    </main>
+  )
+}
+
+export default ArticleList
